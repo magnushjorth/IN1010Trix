3b4d2e88a5352f76913bd5e07a7f78bea4519361
Magnus Hjorth <57001299+magnushjorth@users.noreply.github.com>
2020-01-02 21:33:00 +0100

    02.01.2020

--- TestBaathus.java
+++ TestBaathus.java
@@ -10,7 +10,7 @@ class Baat {
   }
 
   public String hentInfo() {
-    return "Produksjonsnummer: " + prodnr + ", merke: " + merke;
+    return "Produksjonsnummer: " + prodnr + "\nMerke: " + merke;
   }
 }
 
@@ -20,49 +20,45 @@ class Baathus {
   public Baathus(int antallPlasser) {
     baathus = new Baat[antallPlasser];
   }
-  public void settInn(Baat enBaat) {
 
-    boolean settInn = false;
+  public void settInn(Baat enBaat) {
+    boolean sattInn = false;
     int teller = 0;
 
-    while (teller < baathus.length && settInn == false) {
-      if (baathus[teller] == null){
-          baathus[teller] = enBaat;
-          settInn = true;
+    while (teller < baathus.length && sattInn == false) {
+      if (baathus[teller] == null) {
+        baathus[teller] = enBaat;
+        sattInn = true;
       }
+
       teller++;
     }
 
-    if (settInn != true) {
-            System.out.println("Det er ikke plass til flere baater!");
+    if (sattInn != true) {
+      System.out.println("Det er ikke plass til flere baater!\n");
     }
-
   }
-
   public void skrivBaater() {
     for (int i = 0; i < baathus.length; i++) {
-      if (baathus[i] != null){
-        System.out.println(baathus[i].hentInfo());
+      System.out.println(baathus[i].hentInfo());
       }
     }
   }
-}
 
-class TestBaathus {
-  public static void main
-  (String[] args) {
-    Baathus baathus = new Baathus(3);
+class TestBaatHus {
+  public static void main(String[] args) {
+    Baat en = new Baat("Askeladden");
+    Baat to = new Baat("Finnmaster");
+    Baat tre = new Baat("Nordkapp");
+    //Baat fire = new Baat("Mercury");
 
-    Baat b1 = new Baat("Triton");
-    Baat b2 = new Baat("Chaparral");
-    Baat b3 = new Baat("Mercury");
-    Baat b4 = new Baat("Mercury");
+    Baathus baathuset = new Baathus(3);
 
-    baathus.settInn(b1);
-    baathus.settInn(b2);
-    baathus.settInn(b3);
-    baathus.settInn(b4);
+    baathuset.settInn(en);
+    baathuset.settInn(to);
+    baathuset.settInn(tre);
+    //baathuset.settInn(fire);
 
-    baathus.skrivBaater();
+    baathuset.skrivBaater();
   }
 }
