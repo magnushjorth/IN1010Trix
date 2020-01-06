05c06b98c546814baf50bb9fcec96e3dc8e0a275
Magnus Hjorth <57001299+magnushjorth@users.noreply.github.com>
2020-01-06 21:41:34 +0100

    06.01.2020

---
+++ Telefonbok.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+import easyIO.*;
+
+class Telefonbok {
+  public static void main(String[] args) {
+    HashMap<String, String> telefonliste = new HashMap<String, String>();
+
+    telefonliste.put("Arne", "22334455");
+    telefonliste.put("Lisa", "95959595");
+    telefonliste.put("Jonas", "97979797");
+    telefonliste.put("Peder", "12345678");
+
+    In tast = new In();
+    Out skjerm = new Out();
+
+    skjerm.out("Navn: ");
+    String person = tast.inLine();
+
+    System.out.println(telefonliste.get(person));
+  }
+}
